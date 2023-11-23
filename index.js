1fad2f78ee42ded401ea21143b16053bda6ddf0a
Vitor Gabriel <120761175+vitorgrs@users.noreply.github.com>
2023-11-23 11:28:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -40,6 +40,27 @@ app.post('/inserirResposta', async (req, res) => {
   }
 });
 
+aapp.set('view engine', 'ejs'); // ou o motor de template que você está usando
+app.set('views', path.join(__dirname, 'views')); // certifique-se de ajustar o caminho
+
+// Rota para obter os detalhes da denúncia com base no ID
+app.get('/denuncias/:id', async (req, res) => {
+  const idDaDenuncia = req.params.id;
+  const query = 'SELECT * FROM denuncias WHERE id = $1';
+  const values = [idDaDenuncia];
+
+  try {
+    const result = await client.query(query, values);
+    const denuncia = result.rows[0];
+
+    // Renderiza a página feedback.html com os dados da denúncia
+    res.render('feedback', { denuncia });
+  } catch (error) {
+    console.error('Erro ao buscar dados da denúncia:', error);
+    res.status(500).send('Erro ao buscar dados da denúncia');
+  }
+});
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Servidor está ouvindo na porta ${PORT}`);
