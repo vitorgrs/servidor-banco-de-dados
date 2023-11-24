131fc1647baf59b1d13a60426284ef5854cf88bc
Vitor Gabriel <120761175+vitorgrs@users.noreply.github.com>
2023-11-24 11:20:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -41,9 +41,14 @@ app.post('/inserirResposta', async (req, res) => {
 app.get('/obterDenuncia/:protocolo', async (req, res) => {
   const protocolo = req.params.protocolo;
 
+  if (!protocolo) {
+    res.status(400).send('Protocolo não fornecido');
+    return;
+  }
+
   try {
     const result = await client.query('SELECT * FROM denuncias WHERE id = $1', [protocolo]);
-    
+
     if (result.rows.length > 0) {
       const denuncia = result.rows[0];
       res.status(200).json(denuncia);
