683573e09d41a2943520e65a5d5e7501becdd85d
Vitor Gabriel <120761175+vitorgrs@users.noreply.github.com>
2023-11-26 14:24:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -217,16 +217,14 @@ app.get('/getInstagramFeed', async (req, res) => {
 // Rota para obter dados do Twitter
 app.get('/getTwitterFeed', async (req, res) => {
     try {
-        const response = await axios.get('https://api.twitter.com/2/tweets?tweet.fields=text,created_at&user.fields=profile_image_url,username', {
-            headers: {
-                Authorization: `Bearer ${twitterBearerToken}`,
-            },
-        });
-        res.json(response.data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Erro ao obter dados do Twitter');
-    }
+        const response = await axios.get('https://api.twitter.com/2/tweets/search/recent', {
+    headers: {
+        Authorization: `Bearer ${twitterBearerToken}`,
+    },
+    params: {
+        tweet.fields: 'text,created_at',
+        user.fields: 'profile_image_url,username',
+    },
 });
 
 const PORT = process.env.PORT || 3001;
